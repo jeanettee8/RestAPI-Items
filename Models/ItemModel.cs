a11befc4d004cfe14cfe690b436200ea7a186c02
jeanettee8 <ekje112024@gmail.com>
2025-03-27 13:27:01 +0100

    init

---
+++ Models/ItemModel.cs
@@ -0,0 +1,26 @@
+
+
+using Microsoft.AspNetCore.Http.Features;
+
+namespace RestAPI_Items.Models;
+
+public class ItemModel
+{
+    public string? ItemName {get; set;}
+    public double? ItemPrice {get; set;}
+    public int? ItemCount {get; set;}
+    public char? ItemFabric {get; set;} // C - cotton, P - polyester, A - acrylic, W - wool
+    public bool ItemWillRestock {get; set;}
+}
+
+
+/* made a list but then i realized i could try to make my own csv file
+public class ItemLIst
+{
+    public static List<ItemModel> Items = new List<ItemModel>()
+    {
+        
+    }
+
+}
+*/
